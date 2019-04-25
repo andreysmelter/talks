c50f8aa00259dd0df768d6c9314e1fd03bd3f55c
Andrey Smelter <andrey.smelter@gmail.com>
2019-04-25 01:26:51 -0400

    Add code example 5.

---
+++ 2019-04-25-DerbyPy-DocumentingPythonPackages/examples/5_example/equations/quadratic.py
@@ -0,0 +1,31 @@
+"""
+This module contains routines for finding 
+roots of quadratic equations.
+"""
+
+import cmath
+
+
+def quadratic(a, b, c):
+    """Solve quadratic equation.
+
+    For example:
+    >>> x1, x2 = quadratic(a=2, b=-8, c=-24)
+    >>> x1
+    (8+0j)
+    >>> x2
+    (-2+0j)
+    >>> 2*x1**2 - 8*x1 - 24
+    0j
+    >>> 2*x2**2 - 8*x2 - 24
+    0j
+    """
+    discriminant = cmath.sqrt(b**2.0 - 4.0 * a * c)
+    x1 = (-b + discriminant) / (2.0 * a)
+    x2 = (-b - discriminant) / (2.0 * a)
+    return x1, x2
+
+
+if __name__ == "__main__":
+    import doctest
+    print(doctest.testmod())
